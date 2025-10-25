d74d44cbb012d8fba4cddae12318a498d200b1d4
Emoncr <biplobemon75945@gmail.com>
2025-10-25 15:46:49 +0600

    fix: Fixed cors issue

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ app.use(
       'http://localhost:3000',
       'https://devfoliyo.vercel.app',
       'https://devfoliyo.vercel.app',
+      'https://dev-foliyo.vercel.app',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
@@ -24,8 +25,6 @@ app.use(
 );
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
-
-
 
 app.use('/api/health', (req, res) => {
   return res.status(200).json({ message: 'Server is healthy' });
