bc783f895ada730b80d5d13980420a39c4a1b89f
Emoncr <biplobemon75945@gmail.com>
2026-03-29 00:15:12 +0600

    fix: Add additional CORS origins for improved access control

--- src/app.js
+++ src/app.js
@@ -20,6 +20,8 @@ app.use(
       'https://devfoliyo.vercel.app',
       'https://dev-foliyo.vercel.app',
       'https://emondev.vercel.app',
+      'https://www.heyemon.dev',
+      'https://heyemon.dev',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
